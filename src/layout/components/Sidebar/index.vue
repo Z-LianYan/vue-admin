3c0e2152a9ea8d50c22643b3d4056208320a7851
zly <1371935318@qq.com>
2019-12-04 23:30:52 +0800

    修改bug

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -32,60 +32,11 @@ import variables from "@/styles/variables.scss";
 export default {
   components: { SidebarItem, Logo },
   data() {
-    return {
-      // router_menu: [
-      //   {
-      //     path: "/system",
-      //     component: ()=>import('@/layout'),
-      //     redirect: "/system/manager",
-      //     meta: { title: "系统设置", icon: "el-icon-s-tools" },
-      //     children: [
-      //       //管理员
-      //       {
-      //         path: "manager",
-      //         component: ()=>import('@/views/system/manager/index'),
-      //         meta: { title: "管理员管理", icon: "el-icon-s-management" }
-      //       }, //管理员
-      //       {
-      //         path: "manager/add",
-      //         component: ()=>import('@/views/system/manager/add'),
-      //         meta: { title: "添加管理员" },
-      //         hidden: true
-      //       }, //添加管理员
-      //       {
-      //         path: "manager/edit/:id",
-      //         component: ()=>import('@/views/system/manager/edit'),
-      //         meta: { title: "编辑管理员", icon: "dashboard" },
-      //         hidden: true
-      //       }, //编辑管理员
-
-      //       //角色管理
-      //       {
-      //         path: "role",
-      //         component: ()=>import('@/views/system/role/index'),
-      //         meta: { title: "角色管理", icon: "el-icon-user" }
-      //       },
-      //       {
-      //         path: "role/add",
-      //         component: ()=>import('@/views/system/role/add'),
-      //         meta: { title: "添加角色" },
-      //         hidden: true
-      //       },
-      //       {
-      //         path: "role/edit/:id",
-      //         component: ()=>import('@/views/system/role/edit'),
-      //         meta: { title: "编辑角色" },
-      //         hidden: true
-      //       }
-      //     ]
-      //   }
-      // ]
-    };
+    return {};
   },
   computed: {
     ...mapGetters(["sidebar"]),
     routes() {
-      // console.log("this.$router.options.routes", this.$router.options.routes);
       console.log("this.$router.options.routes----+++++++", this.$store.state.accessMenu.routerMenu);
       return this.$router.options.routes.concat(this.$store.state.accessMenu.routerMenu);
     },
